3491a6f93db2597c0215772cecd35aff2bdd1a10
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2022-09-04 11:47:42 -0500

    add splash activity with lottie animation

---
+++ feature-marvel-characters/src/main/java/com/cristhianbonilla/feature_marvel_characters/MarvelActivity.kt
@@ -0,0 +1,11 @@
+package com.cristhianbonilla.feature_marvel_characters
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MarvelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marvel)
+    }
+}
