4dfe59b970db6ffb4b59e4631fddfcbf6d50b0a5
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2022-09-05 00:35:02 -0500

    create binding extensions

--- feature-marvel-characters/src/main/java/com/cristhianbonilla/feature_marvel_characters/MarvelActivity.kt
+++ feature-marvel-characters/src/main/java/com/cristhianbonilla/feature_marvel_characters/MarvelActivity.kt
@@ -2,10 +2,13 @@ package com.cristhianbonilla.feature_marvel_characters
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cristhianbonilla.feature_marvel_characters.databinding.ActivityMarvelBinding
+import com.cristhianbonilla.support.config.activityBinding
 
 class MarvelActivity : AppCompatActivity() {
+    private val binding by activityBinding<ActivityMarvelBinding>(R.layout.activity_marvel)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_marvel)
     }
 }
