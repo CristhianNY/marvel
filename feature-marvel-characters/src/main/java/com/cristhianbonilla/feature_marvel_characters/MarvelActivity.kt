e5006f3e2114c5d8d13e79dd76d5e916cb19e9c5
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2022-09-05 01:15:05 -0500

    show fragment with navigation

--- feature-marvel-characters/src/main/java/com/cristhianbonilla/feature_marvel_characters/MarvelActivity.kt
+++ feature-marvel-characters/src/main/java/com/cristhianbonilla/feature_marvel_characters/MarvelActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.cristhianbonilla.feature_marvel_characters.databinding.ActivityMarvelBinding
 import com.cristhianbonilla.support.config.activityBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MarvelActivity : AppCompatActivity() {
     private val binding by activityBinding<ActivityMarvelBinding>(R.layout.activity_marvel)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.run {
+            setContentView(root)
+        }
     }
 }
